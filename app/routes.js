d6180dc4eef9ffa4fb92b5971a6f35e2ea05af8a
Jun Zhu <zhujunfirst1@gmail.com>
2014-06-18 15:12:06 -0700

    huge updates, find a new package angularjs-nvd3-directives, the charts are pretty good, I have a set of running charts

--- app/routes.js
+++ app/routes.js
@@ -77,6 +77,6 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./public/bootstrapTest.html'); //res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 };
